d2930b975b5b232297db322dc3bd02878ef2db6a
joeyzenghuan <joeyzenghuan@outlook.com>
2023-04-12 00:26:20 +0800

    azure oai

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -1,5 +1,7 @@
 import openai
 from config import Config
+from colorama import Fore, Style
+
 cfg = Config()
 
 openai.api_key = cfg.openai_api_key
@@ -8,9 +10,22 @@ openai.api_key = cfg.openai_api_key
 def create_chat_completion(messages, model=None, temperature=None, max_tokens=None)->str:
     """Create a chat completion using the OpenAI API"""
     if cfg.use_azure:
+        openai.api_type = "azure"
+        # openai.api_version = "2023-03-15-preview" 
+        openai.api_version = cfg.openai_api_version
+        openai.api_base = cfg.openai_api_base
+
+        print("=========================================")
+        for msg in messages:
+            print(Fore.BLUE + f"{msg['role']}: {msg['content']}")
+            print("-----------------------------------------")
+        print("=========================================")
+
+
         response = openai.ChatCompletion.create(
-            deployment_id=cfg.openai_deployment_id,
-            model=model,
+            # deployment_id=cfg.openai_deployment_id,
+            engine=cfg.openai_deployment_id,
+            # model=model,
             messages=messages,
             temperature=temperature,
             max_tokens=max_tokens
@@ -23,4 +38,6 @@ def create_chat_completion(messages, model=None, temperature=None, max_tokens=No
             max_tokens=max_tokens
         )
 
-    return response.choices[0].message["content"]
+    print(Fore.GREEN + response['choices'][0]['message']['content'])
+    return response['choices'][0]['message']['content']
+    # return response.choices[0].message["content"]
--- scripts/memory/base.py
+++ scripts/memory/base.py
@@ -2,11 +2,23 @@
 import abc
 from config import AbstractSingleton
 import openai
+from config import Config
+cfg = Config()
 
 
 def get_ada_embedding(text):
     text = text.replace("\n", " ")
-    return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+    
+    # return openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+
+    openai.api_type = "azure"
+    openai.api_key = cfg.openai_api_key
+    # print("aaa")
+    # print(cfg.openai_api_base)
+    # print(cfg.openai_api_key)
+    openai.api_base = cfg.openai_api_base
+    openai.api_version = "2022-12-01"
+    return openai.Embedding.create(input=[text], engine="text-embedding-ada-002")["data"][0]["embedding"]
 
 
 class MemoryProviderSingleton(AbstractSingleton):
